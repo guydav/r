a9c6254b31924d1f94569d7057292098cd0f9c0b
Guy Davidson <guydav@gmail.com>
2016-10-08 18:36:32 +0200

    Added the data-framed implementation of the Fisher exact test, which is so much prettier

--- 6.1/fisher.R
+++ 6.1/fisher.R
@@ -1,6 +1,3 @@
-
-
-
 fisher.exact <- function(treatment, control, sharp.null, num.experiments) {
     results <- vector("double")
     len.treatment = length(treatment)
@@ -38,9 +35,36 @@ fisher.exact <- function(treatment, control, sharp.null, num.experiments) {
     return(1 - sum(observed.test.statistic > results) / num.experiments)
 }
 
+fisher.df <- function(treatment, control, sharp.null, num.experiments) {
+    results <- vector("double")
+    len.treatment = length(treatment)
+    len.control = length(control)
+    
+    df.treatment = data.frame(y=children.treatment, w=rep(TRUE, len.treatment))
+    df.control = data.frame(y=children.control, w=rep(FALSE, len.control))
+    df = rbind(df.treatment, df.control)
+    rows = nrow(df)
+            
+    for(exp in 1:num.experiments) {
+        t.indices = sample(1:rows, len.treatment)
+        t.values = df[t.indices,'y'] + (!df[t.indices,'w']) * sharp.null 
+        c.values = df[-t.indices,'y'] - df[t.indices,'w'] * sharp.null 
+        
+        test.statistic = mean(t.values) - mean(c.values)
+        # cat(exp, t.indices, t.values, c.values, test.statistic)
+        # print("")
+        results = c(results, test.statistic) 
+    }
+    
+    observed.test.statistic = mean(treatment) - mean(control)
+    return(1 - sum(observed.test.statistic > results) / num.experiments)
+}
+
+
 children.treatment = c(70.0, 66.0, 78.9)
 children.control = c(55.0, 72.0, 72.7)
-fisher.exact(children.treatment, children.control, 0, 1000)
+# fisher.exact(children.treatment, children.control, 0, 1000)
+fisher.df(children.treatment, children.control, 0, 100)
 
 library("Matching")
 library("rgenoud")
